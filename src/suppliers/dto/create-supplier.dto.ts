1b0ff462b761f6c3503acb65a69fac719039b65e
rafaelrabelodasilva <rafaelrabelodasilva@outlook.com>
2023-08-09 17:42:43 -0300

    Subindo estrutura conforme tabela mysql

--- src/suppliers/dto/create-supplier.dto.ts
+++ src/suppliers/dto/create-supplier.dto.ts
@@ -1,3 +1,7 @@
+// os DTOs são amplamente utilizados para validar e estruturar os dados que fluem através das APIs
+// Validação de Dados: Os DTOs permitem que você defina regras de validação diretamente nas classes, usando decoradores da biblioteca class-validator. 
+// Estruturação dos Dados: Ao usar DTOs, você pode definir explicitamente quais campos são esperados em cada solicitação ou resposta.
+
 import { IsEmail, IsInt, IsString, Min, MinLength } from 'class-validator';
 
 export class CreateSupplierDto {
