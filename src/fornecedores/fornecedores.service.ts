7a1fbd169d2e9391025858ee9f0c18aafa12e690
rafaelrabelodasilva <rafaelrabelodasilva@outlook.com>
2023-08-13 19:23:33 -0300

    Adicionado método get all

--- src/fornecedores/fornecedores.service.ts
+++ src/fornecedores/fornecedores.service.ts
@@ -4,9 +4,11 @@ import { AtualizarFornecedorDto } from './dto/atualizar-fornecedor.dto';
 import { Fornecedores } from './entities/fornecedor.entity';
 import { Repository } from 'typeorm';
 
+// o serviço fornecedoresService é definido como um Injectable do NestJS, o que significa que ele pode ser injetado (usado) em outras partes da aplicação que dependem dele
 @Injectable()
 export class fornecedoresService {
 
+    // obtendo uma instância do repositório que permite realizar operações de acesso ao banco de dados para a entidade Fornecedores.
     constructor(
         @Inject('FORNECEDORES_REPOSITORY')
         private fornecedorRepository: Repository<Fornecedores>,
@@ -14,35 +16,6 @@ export class fornecedoresService {
 
     async findAll(): Promise<Fornecedores[]> {
         return this.fornecedorRepository.find();
-    }
-
-
-    private fornecedores: Fornecedores[] = [];
-
-    create(criarFornecedorDto: CriarFornecedorDto) {
-        const currentMaxId = this.fornecedores[this.fornecedores.length - 1]?.id || 0;
-
-        const id = currentMaxId + 1;
-
-        const fornecedor = {
-            id,
-            ...criarFornecedorDto,
-        };
-
-        this.fornecedores.push(fornecedor);
-        return fornecedor;
-    }
-
-    // findAll() {
-    //     return this.fornecedores;
-    // }
-
-    findOne(id: number) {
-        const index = this.fornecedores.findIndex(
-            (fornecedor) => fornecedor.id === id
-        );
-
-        return this.fornecedores[index];
     }
 
     update(id: number, atualizarFornecedorDto: AtualizarFornecedorDto) {
@@ -60,6 +33,39 @@ export class fornecedoresService {
         this.fornecedores[index] = novoFornecedor;
         return novoFornecedor;
     }
+
+    async create(criarFornecedorDto: CriarFornecedorDto): Promise<Fornecedores> {
+        const fornecedor = this.fornecedorRepository.create(criarFornecedorDto);
+        return this.fornecedorRepository.save(fornecedor);
+    }
+
+
+    private fornecedores: Fornecedores[] = [];
+
+
+    findOne(id: number) {
+        const index = this.fornecedores.findIndex(
+            (fornecedor) => fornecedor.id === id
+        );
+
+        return this.fornecedores[index];
+    }
+
+    // update(id: number, atualizarFornecedorDto: AtualizarFornecedorDto) {
+    //     const fornecedor = this.findOne(id);
+
+    //     const novoFornecedor = {
+    //         ...fornecedor,
+    //         ...atualizarFornecedorDto,
+    //     };
+
+    //     const index = this.fornecedores.findIndex(
+    //         (fornecedor) => fornecedor.id === id
+    //     );
+
+    //     this.fornecedores[index] = novoFornecedor;
+    //     return novoFornecedor;
+    // }
 
     remove(id: number) {
         const index = this.fornecedores.findIndex(
